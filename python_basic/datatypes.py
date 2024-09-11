7e6149189868de67917215d7c12e866d92244c41
Emmanuel M <apple@Emmanuel-M.local>
2024-09-11 14:23:07 +0200

    python data types

---
+++ python_basic/datatypes.py
@@ -0,0 +1,26 @@
+# List of data types in python
+# Python has 8 data types that represent the values that can be stored in those values.
+#Fundemental data types
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+# custom types
+    # Classes
+# Specialize data types
+  # Modules  
+  
+None 
+
+# int
+#int is numbers
+
+print(type(2 + 5))
+print(2 - 4)
+print(2 * 4)
+print(2 / 5)
