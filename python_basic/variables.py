b0cbadfc8daa30bdffd93c1f35e107612017e014
Emmanuel M <apple@Emmanuel-M.local>
2024-09-11 16:18:11 +0200

    variable and assigning value to a variable

---
+++ python_basic/variables.py
@@ -0,0 +1,10 @@
+# Assigning a vaue to a variable in python(bidding)
+user_iq = 190
+user_age = user_iq / 4
+
+a = user_age
+
+PI = 3.14 # constant in capital to mean that it will not change
+
+print(user_age)
+print(a)
