07c1e17894c261e7998ec9c256cc789d2f7325b6
Emmanuel M <apple@Emmanuel-M.local>
2024-09-11 17:05:46 +0200

    string definition and statement

---
+++ python_basic/string.py
@@ -0,0 +1,12 @@
+# String is just a piece of text
+print(type("hi, hello there 24!"))
+username = "supercoder"
+password = "supersecret"
+long_string = '''
+wow
+000 00
+___
+
+'''
+
+print(long_string)
