b45c8b89e8432c90966780ff1f8c38a9168a2672
Emmanuel M <apple@Emmanuel-M.local>
2024-09-12 12:20:23 +0200

    Using escape character and tab

--- python_basic/type_conversion.py
+++ python_basic/type_conversion.py
@@ -1,3 +1,11 @@
 # data type conversion
 print(type(str(100)))
 print(type(int(str(100))))
+
+# Escape sequences
+#weather = 'It's sunny day'
+weather = "It's sunny day"
+weather = "It\'s \"kind of\" sunny \n hope you have a good day!"
+
+
+print(weather)
