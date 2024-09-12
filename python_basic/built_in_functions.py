8c25a1422f7556cb080a9abac7322ea963ec241e
Emmanuel M <apple@Emmanuel-M.local>
2024-09-12 14:51:57 +0200

    string built-in function in python and string methods

--- python_basic/built_in_functions.py
+++ python_basic/built_in_functions.py
@@ -1,8 +1,19 @@
 # Built-in functions in python
-str()
-int()
-print()
-float()
-round()
-abs()
+# str()
+# int()
+# print()
+# float()
+# round()
+# abs()
 
+# string methods in python
+greet = 'helloooo'
+print(greet[0:len(greet)])
+
+quote = 'to be or not to be'
+print(quote.upper())
+print(quote.capitalize())
+print(quote.lower())
+print(quote.find('be'))
+print(quote.replace('be', 'me'))
+print(quote)
