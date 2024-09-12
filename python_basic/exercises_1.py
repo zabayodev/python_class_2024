d00a2f09acd7cc6dcfccd7739b168a482a328919
Emmanuel M <apple@Emmanuel-M.local>
2024-09-12 16:48:55 +0200

    exercises in covered data types and conversion

--- python_basic/exercises_1.py
+++ python_basic/exercises_1.py
@@ -1,6 +1,11 @@
-name = 'Emma zab'
-age = 50
-relationship_status = 'single'
-relationship_status = 'it\'s complicated'
+# name = 'Emma zab'
+# age = 50
+# relationship_status = 'single'
+# relationship_status = 'it\'s complicated'
 
-print(relationship_status)
+# print(relationship_status)
+
+birth_year = input("what year you born?: ")
+current_year = 2024
+age  = current_year - int(birth_year)
+print(f"You are {age} years old")
