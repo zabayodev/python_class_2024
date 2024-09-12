a7badef4129c64e86f47a5157941812764b82bbe
Emmanuel M <apple@Emmanuel-M.local>
2024-09-12 14:27:17 +0200

    string immutability in python

---
+++ python_basic/immutability.py
@@ -0,0 +1,12 @@
+selfish = '01234567'
+
+# Strings in python are immutable
+selfish = 100
+
+
+
+selfish[0] = '8'
+
+print(selfish)
+
+# The only way to change a string is by giving it a new value because of immutability.
