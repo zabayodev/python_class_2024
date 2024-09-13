597925525dd48311669235dcd13c09c7874ba009
Emmanuel M <apple@Emmanuel-M.local>
2024-09-13 17:44:44 +0200

    sets in python

---
+++ python_basic/sets.py
@@ -0,0 +1,12 @@
+# Learning the set
+# unordered collections of itmes
+my_set = {1,2,3,4,5,5}
+print(my_set)
+
+# no duplicate in a set
+my_set.add(2)
+my_set.add(100)
+print(my_set)
+my_list = [1,2,3,4,5,5]
+print(set(my_list))
+print(my_list[0])
