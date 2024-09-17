d01f4cb730be8878b042555ddd84ec76be480107
Emmanuel M <apple@Emmanuel-M.local>
2024-09-17 17:14:47 +0200

    logical operators in python and using not

--- python_basic_2/logical_operator.py
+++ python_basic_2/logical_operator.py
@@ -1,7 +1,19 @@
 # >, <, ==, >=, <=, !=, and, or, not
 
-is_friend = True
-is_user = True
+# is_friend = True
+# is_user = True
 
-if is_friend or is_user:
-    print('best friends forever')
+# if is_friend or is_user:
+#     print('best friends forever')
+    
+is_magician = False
+is_expert = True
+if is_magician and is_expert:
+    print('You are a master magician!')
+elif is_magician and not is_expert:
+    print('At least you are getting there')
+elif not is_magician:
+    print('You need magic powers')
+    
+    
+     
