2674cb9491e3ada0f444cc42a1e899a6da48705f
Emmanuel M <apple@Emmanuel-M.local>
2024-09-17 16:30:18 +0200

    conditional logic in python

--- python_basic_2/conditional_logic.py
+++ python_basic_2/conditional_logic.py
@@ -5,3 +5,11 @@ if is_old:
     print('You are old enough to drive')
 else:
     print('check your age again!')
+    
+# Ternary operator
+''' it is a conditional logic where all the conditions and if clause are on the same line'''
+# condition_if_tru if condition ense condition_if_false
+
+is_friend = True
+can_message = "message allowed" if is_friend else "not allowed to message"
+print(can_message)
