c3428f7801728cd788aefbf6f4666fd2f8a77def
Emmanuel M <apple@Emmanuel-M.local>
2024-09-19 14:13:30 +0200

    writing a DRY function in python

---
+++ python_basic_2/functions.py
@@ -0,0 +1,15 @@
+# Developing functions in python
+# using DRY(Do not Repeat Yourself)
+# def say_hello():
+#     print('helloooo')
+    
+# say_hello()
+
+# parameters and arguments
+def say_hello(name, emoji): # parameters in the functions
+    print(f'helloooo {name} {emoji}')
+    
+# Arguments
+say_hello('Emma', '**')
+say_hello('Dan', '**')
+say_hello('Emily', '**')
