77b3437eb0677fdda06b852913fa2337b3ad08cc
Emmanuel M <apple@Emmanuel-M.local>
2024-09-19 11:18:46 +0200

    using while loop in python

---
+++ python_basic_2/while_loop.py
@@ -0,0 +1,15 @@
+# using while loop in python
+# using an infinite loop
+# i = 0
+# while 0 < 50:
+#     print(i)
+
+# i = 0
+# while 0 < 50:
+#     print(i)
+#     break
+
+i = 0
+while i < 50:
+    print(i)
+    i = i + 1
