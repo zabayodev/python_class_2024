f1051fee41c83098883dca05bdc25f39de8984c5
Emmanuel M <apple@Emmanuel-M.local>
2024-09-19 15:13:04 +0200

    writing a DRY function in python

---
+++ python_basic_2/method_functions.py
@@ -0,0 +1,12 @@
+# Methods and functions
+# there are different built in functins
+# user defined functions using def
+# the method use the dot notation
+
+print('hello'.capitalize())
+
+# docstring for a functions
+'''
+This function produce the test use cases in python
+
+'''
